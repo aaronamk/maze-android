6325ad9e52b9d570827c4724332326f3dd63ca9a
aaronamk <aaronamk@gmail.com>
2018-06-24 15:21:47 -0400

    More Button Interaction.

---
+++ app/src/main/java/com/example/aaron/test/FinishActivity.java
@@ -0,0 +1,18 @@
+package com.example.aaron.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+
+public class FinishActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_finish);
+    }
+    public void switchToMenu(){
+        Intent intent = new Intent(this, AMazeActivity.class);
+        startActivity(intent);
+    }
+}
--- app/src/main/java/com/example/aaron/test/GeneratingActivity.java
+++ app/src/main/java/com/example/aaron/test/GeneratingActivity.java
@@ -1,13 +1,55 @@
 package com.example.aaron.test;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
 
 public class GeneratingActivity extends AppCompatActivity {
+    private ProgressBar LoadBar;
+    private int progress = 0;
+    private Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_generating);
+
+        LoadBar = findViewById(R.id.load_bar);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(progress < 100){
+                    progress++;
+                    android.os.SystemClock.sleep(50);
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            LoadBar.setProgress(progress);
+                        }
+                    });
+                }
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        switchToManual();
+                    }
+                });
+            }
+        }).start();
+    }
+    public void switchToTitle(View view){
+        startActivity(new Intent(this, AMazeActivity.class));
+    }
+
+    public void switchToManual(){
+        startActivity(new Intent(this, PlayManuallyActivity.class));
+    }
+
+    public void switchToAnimation(){
+        startActivity(new Intent(this, PlayAnimationActivity.class));
     }
 }
